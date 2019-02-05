3dfe358961c89ac572709ad75286294c1c341027
marwajomaa <marwajomaa4@gmail.com>
2019-02-06 01:59:33 +0200

    create store

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import {Provider} from 'react-redux'
-import store from '../redux/store'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+// store take 3 things (rootReduces, initailState, middleware)
+import thunk from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
+import rootReduces from './reducers'
+const initailState = {};
+const middleware = [thunk]
+
+export const store = createStore(rootReduces, initailState, applyMiddleware(...middleware))
