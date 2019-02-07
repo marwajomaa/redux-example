f3445fe4cc3c47b9552056b85276930c854c5103
marwajomaa <marwajomaa4@gmail.com>
2019-02-07 02:26:29 +0200

    create action with type and payload

---
+++ src/redux/actions/postsAction.js
@@ -0,0 +1,13 @@
+import { FETCH_POSTS, NEW_POST } from './types'
+
+const fetchPosts = ()=> dispatch =>{
+  fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(res=>res.json())
+  .then(posts=>
+    dispatch({
+    type: FETCH_POSTS,
+    payload: posts
+  }))
+}
+
+export default fetchPosts
